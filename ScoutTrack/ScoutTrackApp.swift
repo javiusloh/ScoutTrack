3671c5aaadb15d1e4d6a28093b3c954783ce2e37
Javius Loh Jingwei <145333430+javiusloh@users.noreply.github.com>
2025-05-26 16:38:37 +0800

    Initial Commit

---
+++ ScoutTrack/ScoutTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutTrackApp.swift
+//  ScoutTrack
+//
+//  Created by Javius Loh on 26/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoutTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
